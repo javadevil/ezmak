7d39c3f57ac0d8a46c70e2fd7d3f900a8db586e2
javadevil <n.Kimmy@gmail.com>
2016-06-13 10:07:52 +0700

    add node server serve

---
+++ webapplication/index.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const router = require('express').Router();
+const path = require('path');
+const serv = require('serve-static');
+
+module.exports = function index() {
+	router.use(serv(path.join(__dirname,'build')),{index:['index.html']});
+	return router;
+}
